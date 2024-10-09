30a389f3750cc6a8973a90a4dfdaef5e764437bd
marcojob <44396071+marcojob@users.noreply.github.com>
2024-10-09 09:01:45 +0200

    bayer: Run demosaicing on GPU

--- include/usb_cam/formats/bayer.hpp
+++ include/usb_cam/formats/bayer.hpp
@@ -6,6 +6,8 @@
 #include "usb_cam/formats/utils.hpp"
 
 #include <opencv2/opencv.hpp>
+#include <opencv2/cudaimgproc.hpp>
+#include <opencv2/cudaarithm.hpp>
 
 namespace usb_cam
 {
@@ -24,7 +26,9 @@ public:
         8,
         true), // True indicates that this needs a conversion to RGB8
     _height(args.height),
-    _width(args.width) {
+    _width(args.width),
+    _use_cuda(cv::cuda::getCudaEnabledDeviceCount() > 0)
+    {
         _rgb8_bytes = 3 * _height * _width;
         _reallocate_images();
     }
@@ -35,21 +39,27 @@ public:
     /// @param bytes_used number of bytes used by source image
     void convert(const char* &src, char* &dest, const int& bytes_used) override {
         (void)bytes_used;
-        // https://www.kernel.org/doc/html/v4.9/media/uapi/v4l/pixfmt-srggb10.html
-        // In 10 bit Bayer GRBG format
-        // Each row has 2 * width pixels
-        // Only reallocate if the size changes
         _reallocate_images();
         _bayer_image.data = (uchar*)src;
 
-        // Demosaic and convert to 8 bit mat
-        cv::demosaicing(_bayer_image, _rgb_image, cv::COLOR_BayerGR2RGB);
+        if (_use_cuda) {
+            cv::_gpu_bayer_image.upload(_bayer_image);
+            cv::cuda::demosaicing(_gpu_bayer_image, _gpu_rgb_image, cv::COLOR_BayerGR2RGB);
+            _gpu_rgb_image.download(_rgb_image);
+
+        } else {
+            cv::demosaicing(_bayer_image, _rgb_image, cv::COLOR_BayerGR2RGB);
+        }
+
         _rgb_image.convertTo(_rgb_image_8bit, CV_8U, _scaling_16_to_8);
         std::memcpy(dest, _rgb_image_8bit.data, _rgb8_bytes);
     }
 
 private:
+    bool _use_cuda = false;
     const double _scaling_16_to_8 = 256.0 / 65536.0;
+    cv::cuda::GpuMat _gpu_bayer_image;
+    cv::cuda::GpuMat _gpu_rgb_image;
     cv::Mat _bayer_image;
     cv::Mat _rgb_image_8bit;
     cv::Mat _rgb_image;
@@ -58,11 +68,17 @@ private:
     int _width = 0;
 
     void _reallocate_images() {
-        if (_bayer_image.rows != _height || _bayer_image.cols != _width ||
-            _rgb_image.rows != _width || _rgb_image.cols != _width) {
+        // Simplification of the condition: All frames have the same size
+        if (_bayer_image.rows != _height || _bayer_image.cols != _width) {
             _bayer_image = cv::Mat(_height, _width, CV_16UC1);
-            _rgb_image = cv::Mat(_height, _width, CV_16UC1);
-        }
+            _rgb_image = cv::Mat(_height, _width, CV_16UC3);
+            _rgb_image_8bit = cv::Mat(_height, _width, CV_8UC3);
+
+            if (_use_uda)
+                _gpu_bayer_image = cv::cuda::GpuMat(_height, _width, CV_16UC1);
+                _gpu_rgb_image = cv::cuda::GpuMat(_height, _width, CV_16UC3);
+            }
+
     }
 };
 
